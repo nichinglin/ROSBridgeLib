35823031cea7b3567c871e6dee9f7b5abe6f16e2
nichinglin <mo83117401500094@gmail.com>
2017-12-13 14:59:41 +0800

    add Msg Float64 and Float64MultiArray

---
+++ Assets/ROS_LIB/std_msgs/Float64Msg.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Text;
+using SimpleJSON;
+
+namespace ROSBridgeLib {
+	namespace std_msgs {
+		public class Float64Msg : ROSBridgeMsg {
+			private float _data;
+			
+			public Float64Msg(JSONNode msg) {
+				_data = float.Parse(msg["data"]);
+			}
+			
+			public Float64Msg(float data) {
+				_data = data;
+			}
+			
+			public static string GetMessageType() {
+				return "std_msgs/Float64";
+			}
+			
+			public float GetData() {
+				return _data;
+			}
+			
+			public override string ToString() {
+				return "Float64 [data=" + _data + "]";
+			}
+			
+			public override string ToYAMLString() {
+				return "{\"data\" : " + _data + "}";
+			}
+		}
+	}
+}
